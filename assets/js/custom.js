377f0c68de9523c92cc648c48f50debfa7e5121c
Sarath <sarathlalps08@gmail.com>
2025-03-16 23:41:12 +0530

    Initial commit

---
+++ assets/js/custom.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the search icon element
+    var searchIcon = document.getElementById('searchIcon');
+    // Get the search bar element
+    var searchBar = document.getElementById('searchBar');
+
+    // Add a click event listener to the search icon
+    searchIcon.addEventListener('click', function (event) {
+        // Prevent the default link behavior
+        event.preventDefault();
+
+        // Toggle the "d-none" class on the search bar
+        searchBar.classList.toggle('d-none');
+    });
+});
